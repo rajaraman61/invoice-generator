f1cd73671aa078060034fb2d03611431a67e2c24
rajaraman61 <rajaraman61@outlook.com>
2022-09-18 11:05:44 +0530

    Invocide perfo

--- src/services/invoice_handler.py
+++ src/services/invoice_handler.py
@@ -1,13 +1,12 @@
-from time import sleep
 from core.errors import InvoiceException
 from loguru import logger
 from jinja2 import Environment, FileSystemLoader
 import pdfkit
 import os
-import shutil
 
 class InvoiceHandler(object):
 
+    
     @classmethod
     def generate_invoice(self, file):
         if file is not None:
@@ -17,20 +16,15 @@ class InvoiceHandler(object):
                 logger.info(item)
                 filename = item['bill_no']
                 record = [item]
-                self.generate_pdf_css('/html/templates/template.j2', 'style.css', f'../invoices/invoice{filename}.pdf', input_file=record)
+                self.generate_pdf_css('/html/templates/template.j2', 'style.css', f'invoice{filename}.pdf', input_file=record)
             return template_record
         else:
             raise InvoiceException(f"'{file}' url is missing")
-        
-
-    
     
     @classmethod
     def render_template(self, filename, input_file):
         current_directory = os.path.dirname(os.path.abspath(__file__))
-        logger.info(current_directory)
         env = Environment(loader=FileSystemLoader(current_directory))
-        logger.info(env)
         return env.get_template(filename).render(
             input_file[0]
         )
